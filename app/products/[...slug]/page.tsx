c2e6720d73a3a42b2509d1e5431ce899f0ffc0b2
Hridaya Raj Rajbanshi <35860420+hridayarajbanshi@users.noreply.github.com>
2025-02-21 21:32:57 +0545

    Add promotional section to product detail page with background image and call-to-action button

--- app/products/[...slug]/page.tsx
+++ app/products/[...slug]/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '@/components/Navbar'
 import Tabs from '@/components/Tabs'
 import Footer from '@/components/Footer'
-import ProductList from '@/components/ProductList'
 import Link from 'next/link'
 const page = ({params, }: {
     params: {slug: string[]}
@@ -76,6 +75,31 @@ const page = ({params, }: {
           </li>
         </ol>
       </nav>
+
+
+<section
+  className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?q=80&w=2670&auto=format&fit=crop)] bg-cover bg-top bg-no-repeat bg-fixed"
+>
+  <div className="bg-black/50 p-8 md:p-12 lg:px-16 lg:py-24">
+    <div className=" ltr:sm:text-left rtl:sm:text-right">
+      <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">Latest Skims</h2>
+
+      <p className="hidden max-w-lg text-white/90 md:mt-6 md:block  md:text-lg md:leading-relaxed">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore officia corporis quasi
+        doloribus iure architecto quae voluptatum beatae excepturi dolores.
+      </p>
+
+      <div className="mt-4 sm:mt-8">
+        <a
+          href="#"
+          className="inline-block rounded-full bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:ring-3 focus:ring-yellow-400 focus:outline-hidden"
+        >
+          Get Started
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
 
       
 
