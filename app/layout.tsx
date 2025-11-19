dfe2456d279e0aded35f3039a86116ed041e5375
Hridaya Raj Rajbanshi <35860420+hridayarajbanshi@users.noreply.github.com>
2025-11-19 19:58:48 +0545

    refactor: rename project from "bite-bazzar" to "techcart" and update dependencies
    chore: update PostCSS configuration to use object syntax for plugins
    
    refactor: remove unused provider component for session management
    
    delete: remove logo image from public assets
    
    delete: remove seed script for database initialization
    
    chore: update TypeScript configuration for JSX and include additional file types
    
    delete: remove unused Validator utility
    
    delete: remove admin authentication utility
    
    delete: remove unused money utility

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import { AuthProvider } from "@/providers";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,29 +13,21 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Techcart - Your One-Stop Tech Shop",
-  description: "Discover the latest in tech at Techcart. From cutting-edge gadgets to essential accessories, we have everything you need to stay connected and ahead of the curve.",
+  title: "Techcart - Your Tech Shopping Companion",
+  description: "Discover the best tech deals, reviews, and buying guides with Techcart. Your ultimate destination for all things technology.",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
-  
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
-        <AuthProvider>
-          <Navbar/>
         {children}
-        <Footer/>
-        </AuthProvider>
-        
       </body>
     </html>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[{
-      protocol: 'https',
-      hostname: 'res.cloudinary.com',
-      port: '',
-      pathname: '/dlz6kjzbu/**',
-    }]
-
-  }
   /* config options here */
 };
 
